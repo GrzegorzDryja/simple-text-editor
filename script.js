89b6cc6699b8e0398585f8808e2714b04c5e2a57
GrzegorzDryja <grzegorz.dryja@outlook.com>
2021-05-07 21:49:46 +0200

    Some DRY and left-right, right-left selecting
    Selecting with mouse move or with shift+arrows

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 document.getElementById("editor").addEventListener("click", () => {
-    const selection = window.getSelection();
-    const startOfSelectedText = selection.anchorOffset;
-    const endOfSelectedText = selection.focusOffset;
-    const selectedText = selection.anchorNode.data.slice(startOfSelectedText, endOfSelectedText)
-    console.log(selectedText)
+  const selection = window.getSelection();
+  calculateSelectedText(selection)
 })
+
+document.getElementById("editor").addEventListener("keyup", (event) => {
+  if(event.shiftKey && event.keyCode == 39 || event.keyCode == 37){
+    const selection = window.getSelection();
+    calculateSelectedText(selection)
+  }
+})
+
+function calculateSelectedText(selection){
+  const startOfSelectedText = selection.anchorOffset;
+  const endOfSelectedText = selection.focusOffset;
+  let selectedText;
+  if (startOfSelectedText <= endOfSelectedText){
+    selectedText = selection.anchorNode.data.slice(startOfSelectedText, endOfSelectedText)
+  } else {
+    selectedText = selection.anchorNode.data.slice(endOfSelectedText, startOfSelectedText)
+  }
+  console.log(selectedText)  
+}
