c07311cd1d0b2393f477824e263302b0b4435002
GrzegorzDryja <grzegorz.dryja@outlook.com>
2021-05-08 15:22:53 +0200

    Just toString() :]

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 document.getElementById("editor").addEventListener("click", () => {
-  const selection = window.getSelection();
+  const selection = document.getSelection();
   calculateSelectedText(selection)
 })
 
 document.getElementById("editor").addEventListener("keyup", (event) => {
   if(event.shiftKey && event.keyCode == 39 || event.keyCode == 37){
-    const selection = window.getSelection();  
+    const selection = document.getSelection();  
     calculateSelectedText(selection)
   }
 })
 
 function calculateSelectedText(selection){
-  const startOfSelectedText = selection.anchorOffset;
-  const endOfSelectedText = selection.focusOffset;
-  let selectedText = "";
-  if (startOfSelectedText <= endOfSelectedText && selection.anchorNode == selection.focusNode){
-    selectedText = selection.anchorNode.textContent.slice(startOfSelectedText, endOfSelectedText)
-  }
-  if (startOfSelectedText >= endOfSelectedText && selection.anchorNode == selection.focusNode){
-    selectedText = selection.anchorNode.textContent.slice(endOfSelectedText, startOfSelectedText)
-  }
-  if (selection.anchorNode !== selection.focusNode){
-    selectedText += selection.anchorNode.textContent.substring(startOfSelectedText);
-    selectedText += selection.focusNode.textContent.substring(0, endOfSelectedText)
+  console.log(selection.toString())
+  // const startOfSelectedText = selection.anchorOffset;
+  // const endOfSelectedText = selection.focusOffset;
+  // let selectedText = "";
+  // if (startOfSelectedText <= endOfSelectedText && selection.anchorNode == selection.focusNode){
+  //   selectedText = selection.anchorNode.textContent.slice(startOfSelectedText, endOfSelectedText)
+  // }
+  // if (startOfSelectedText >= endOfSelectedText && selection.anchorNode == selection.focusNode){
+  //   selectedText = selection.anchorNode.textContent.slice(endOfSelectedText, startOfSelectedText)
+  // }
+  // if (selection.anchorNode !== selection.focusNode){
+  //   let range = new Range();
+  //   firstTextNode = selection.anchorNode;
+  //   lastTextNode = selection.focusNode;
+
+  //   range.setStart(firstTextNode, startOfSelectedText);
+  //   range.setEnd(lastTextNode, endOfSelectedText);
+  //   console.log(firstTextNode)
+  //   console.log(range);
+
+
+    // selectedText += selection.anchorNode.textContent.substring(startOfSelectedText);
+    // selectedText += selection.focusNode.textContent.substring(0, endOfSelectedText)
     //First node cated from start... + next node + ... + last node cated to end...
   }
-  console.log(selection)
-  console.log(selectedText)
-}
