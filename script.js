4a6cbdb91ebe6f848a50dd2628d56d09405802ed
GrzegorzDryja <grzegorz.dryja@outlook.com>
2021-05-09 00:15:33 +0200

    new Range, sorroundContents
    user select is lost, and dosn't work on selection like "<b>sfsfa" with no clossing tag
    setStart/setEnd should be mixed to handle left/right way selection

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-document.
-  addEventListener("selectionchange", () => {
-    const selection = document.getSelection();  
-    console.log(selection.toString());
+let selection;
+let bold = false;
+let italic = false;
+let unorderedList = false;
+
+document.addEventListener("selectionchange", () => {
+    selection = document.getSelection();
   })
+
+function makeItBold(){
+  const startOfSelectedText = selection.anchorOffset;
+  const endOfSelectedText = selection.focusOffset;
+  const firstTextNode = selection.anchorNode;
+  const lastTextNode = selection.focusNode;
+
+  let range = new Range();
+  let bNode = document.createElement('b');
+
+  range.setStart(firstTextNode, startOfSelectedText); //only left to right selection in this scenario
+  range.setEnd(lastTextNode, endOfSelectedText)
+
+  let b = document.createElement('b');
+  try {
+    range.surroundContents(b); //Works, but... user select is lost, and dosn't work on selection like "<b>sfsfa" with no clossing tag
+  } catch(e) { console.log(e) }    
+
+  // bNode.innerHTML = selection.toString(); //It lost all other html elements
+  // bNode.innerHTML = range.cloneContents().childNodes[0].textContent;
+  // console.log(range.cloneContents());
+  // selection.removeAllRanges(); //It lost also user select
+  // selection.addRange(range);    
+  // range.insertNode(bNode);
+  // range.deleteContents()
+  bold = true;
+}
+
+function makeItItalic(){
+  italic = true;
+  console.log(selection.toString()) 
+}
+
+function makeUnorderedList(){
+  unorderedList = true;
+  console.log(selection.toString())
+}
