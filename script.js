bab918472b7ec5e8ed14c5055ab618bd0939dedc
GrzegorzDryja <grzegorz.dryja@outlook.com>
2021-05-10 21:15:25 +0200

    One format method

--- script.js
+++ script.js
@@ -7,35 +7,29 @@ document.addEventListener("selectionchange", () => {
     selection = document.getSelection();
   })
 
-function makeItBold(){
-  //Yep... crazy logic for handling left-right/right-left selection
-  const startOfSelectedText = selection.anchorOffset <= selection.focusOffset ? selection.anchorOffset : selection.focusOffset;
-  const endOfSelectedText = startOfSelectedText == selection.anchorOffset ? selection.focusOffset : selection.anchorOffset;
-  const firstTextNode = startOfSelectedText == selection.anchorOffset ? selection.anchorNode : selection.focusNode;
-  const lastTextNode = firstTextNode == selection.anchorNode ? selection.focusNode : selection.anchorNode;
-
-  let range = new Range();
+function format(style){
+  if(style === "b" || style === "i"){
+    //Yep... crazy logic for handling left-right/right-left selection to pass in Range
+    const startOfSelectedText = selection.anchorOffset <= selection.focusOffset ? selection.anchorOffset : selection.focusOffset;
+    const endOfSelectedText = startOfSelectedText == selection.anchorOffset ? selection.focusOffset : selection.anchorOffset;
+    const firstTextNode = startOfSelectedText == selection.anchorOffset ? selection.anchorNode : selection.focusNode;
+    const lastTextNode = firstTextNode == selection.anchorNode ? selection.focusNode : selection.anchorNode;
   
-  range.setStart(firstTextNode, startOfSelectedText);
-  range.setEnd(lastTextNode, endOfSelectedText)
+    let range = new Range();
+    
+    range.setStart(firstTextNode, startOfSelectedText);
+    range.setEnd(lastTextNode, endOfSelectedText)
+    
+    let formatHtmlElement= document.createElement(style);
+    try {
+      range.surroundContents(formatHtmlElement); //Works, but... dosn't work on selection like "<b>sfsfa" with no clossing tag
+    } catch(e) { console.log(e) }    
   
-  let b = document.createElement('b');
-  try {
-    range.surroundContents(b); //Works, but... dosn't work on selection like "<b>sfsfa" with no clossing tag
-  } catch(e) { console.log(e) }    
-
-  selection.removeAllRanges();
-  selection.addRange(range);
-
-  bold = true;
-}
-
-function makeItItalic(){
-  italic = true;
-  console.log(selection.toString()) 
-}
-
-function makeUnorderedList(){
-  unorderedList = true;
-  console.log(selection.toString())
+    selection.removeAllRanges();
+    selection.addRange(range);
+  
+  console.log(`Someday I will ${style} this text... ${range.toString()}`)
+  } else {
+    alert("There is no style match!")
+  }
 }
